cd579a1e12ed1ca245dc8361cbc579ac01401c14
David Martinson <davidmartinson@Davids-Mac-mini.local>
2023-10-15 08:58:47 -0700

    proj 3

---
+++ functions/3-airtable.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const Airtable = require('airtable-node');
+ 
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base('appC63tbWC2RKZmhy')
+  .table('products')
+
+
+exports.handler = async (event, context) => {
+
+    try {
+        const {records} = await airtable.list()
+        const products = records.map((product) => {
+            const {id} = product;
+            const {name, image, price} = product.fields 
+            const url = image[0].url
+            return {id, name, url, price}
+        })
+
+      return {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+        statusCode: 200,
+        body: JSON.stringify(products)
+    }  
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'General Server Error'
+        }
+    }
+
+    
+}
